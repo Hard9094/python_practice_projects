5042f710ac0f0727df2dfd1d3131b060795f0bfd
Hard Patel <135719184+Hard9094@users.noreply.github.com>
2023-11-11 13:22:58 +0530

    Add files via upload
    practicing the python using apis and plots and many other concepts

---
+++ circle.py
@@ -0,0 +1,41 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9df279c2",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "pi=3.14159\n",
+    "def area(r):\n",
+    "    return pi*r**2\n",
+    "def cir(r):\n",
+    "    return pi*r*2\n",
+    "def surface:\n",
+    "    return 4*area(r)\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ circle1.py
@@ -0,0 +1,7 @@
+pi=3.14159
+def area(r):
+    return pi*r**2
+def cir(r):
+    return pi*r*2
+def surface:
+    return 4*pi*r*r
